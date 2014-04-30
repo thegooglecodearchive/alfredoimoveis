a6db3ddb9b7b035247b427779ebc823b875bc0f3
gpzim98 <gpzim98@Servidor.(none)>
2014-04-29 23:59:03 -0300

    Correcoes dentro do servidor

--- alfredo_imoveis/enderecos/models.py
+++ alfredo_imoveis/enderecos/models.py
@@ -19,27 +19,14 @@ class Bairro(models.Model):
     nome = models.CharField(max_length = 150, null = False, blank = False,
                             verbose_name = 'Bairro', help_text='Informe o nome do bairro')
     cidade = models.ForeignKey(Cidade)
-    codigo = models.IntegerField('Código',null = True, blank = True)
 
     def __unicode__(self):
         return self.nome + " - " + self.cidade.nome
 
-def insere_codigo(**kwargs):
-    bairro = kwargs['instance']
-    bairro.codigo = bairro.id
-    models.signals.post_save.disconnect(insere_codigo, sender=Bairro,
-                            dispatch_uid="enderecos.models.Bairro")
-    bairro.save()
-    models.signals.post_save.connect(insere_codigo, sender=Bairro,
-                            dispatch_uid="enderecos.models.Bairro")
-
-models.signals.post_save.connect(insere_codigo, sender=Bairro,
-                            dispatch_uid="enderecos.models.Bairro")
-
 class Endereco(models.Model):
     rua = models.CharField(max_length = 150, null  = False, blank = False)
     numero = models.IntegerField(null = False,  blank = False               )
-    bairro = models.ForeignKey(Bairro, null = False,  blank = False)
+    bairro = models.ForeignKey(Bairro, null = False,  blank = False,default=1)
     cep = models.CharField(max_length = 9,   null  = True, blank = True)
 
     def __unicode__(self):
