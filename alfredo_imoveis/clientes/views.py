e3bec497d46cb74813eded1190807b6da173557a
gpzim98 <gpzim98@gmail.com>
2014-08-02 12:00:24 -0300

    Foi removido o parametro logo do cadastro de parametros, uma vez que agora a logo é colocada no cadastro de empresas, agora a logo é puxada de la e a imagem da carta de aniversaio esta salva na pasta de imagens, pra muda-la tem que contatar o programador

--- alfredo_imoveis/clientes/views.py
+++ alfredo_imoveis/clientes/views.py
@@ -2,12 +2,11 @@
 from django.shortcuts import render, get_object_or_404
 from models import Cliente
 from forms import ClienteForm
-from enderecos.models import Endereco
 from enderecos.forms import EnderecoForm
 from datetime import datetime, date
 from imoveis.models import Imovel
 from funcionarios.models import Funcionario
-from alfredo_imoveis.views import  busca_configuracoes
+from alfredo_imoveis.views import busca_configuracoes
 from parametros.models import ParametrosGerais
 
 today = date.today()
@@ -18,23 +17,26 @@ template_novo = 'clientes/novo.html'
 template_detalhe = 'clientes/detalhe.html'
 template_relatorio = 'clientes/relatorio.html'
 
+
 def home(request):
     dados = {}
     dados['clientes'] = Cliente.objects.filter(ativo=True).order_by('nome')
-    return render(request,template_home,dados)
+    return render(request, template_home, dados)
+
 
 def novo(request):
     dados = {}
     dados['form'] = ClienteForm()
     dados['formEndereco'] = EnderecoForm()
-    return render(request,template_novo,dados)
+    return render(request, template_novo, dados)
+
 
 def salvar(request, id=None):
     dados = {}
 
     form = ClienteForm(request.POST or None)
     formEndereco = EnderecoForm(request.POST or None)
-    
+
     if form.is_valid() and formEndereco.is_valid():
         cliente = form.save(commit=False)
 
@@ -43,7 +45,7 @@ def salvar(request, id=None):
             cliente.data_cadastro = Cliente.objects.get(id=id).data_cadastro
 
         cliente.endereco = formEndereco.save()
-        
+
         empresa = Funcionario.objects.get(usuario=request.user).empresa
         cliente.empresa = empresa
         cliente.ativo = True
@@ -53,10 +55,11 @@ def salvar(request, id=None):
     else:
         dados['form'] = form
         dados['formEndereco'] = formEndereco
-        dados['erros'] = form.errors 
+        dados['erros'] = form.errors
         return render(request, template_novo, dados)
 
-def detalhe(request,id,mensagem=None):
+
+def detalhe(request, id, mensagem=None):
     dados = {}
     dados['mensagem'] = mensagem
     cliente = get_object_or_404(Cliente, id=id)
@@ -67,34 +70,47 @@ def detalhe(request,id,mensagem=None):
     dados['imoveis'] = Imovel.objects.filter(proprietario=cliente)
     return render(request, template_detalhe, dados)
 
-def delete(request,id):
+
+def delete(request, id):
     cliente = get_object_or_404(Cliente, id=id)
     cliente.ativo = False
     cliente.save()
     return home(request)
 
+
 def filtrar(request):
     dados = {}
-    busca_configuracoes(request,dados)
+    busca_configuracoes(request, dados)
     clientes = Cliente.objects.all()
 
     if request.POST['nome']:
-        clientes = clientes.filter(nome__icontains = request.POST['nome'])
+        clientes = clientes.filter(nome__icontains=request.POST['nome'])
     if request.POST['codigo']:
-        clientes = clientes.filter(id__icontains = request.POST['codigo'])
+        clientes = clientes.filter(id__icontains=request.POST['codigo'])
 
     if request.POST['endereco']:
-        if clientes.filter(endereco__rua__icontains = request.POST['endereco']):
-            clientes = clientes.filter(endereco__rua__icontains = request.POST['endereco'])
+        if clientes.filter(endereco__rua__icontains=request.POST['endereco']):
+            clientes = clientes.filter(
+                endereco__rua__icontains=request.POST['endereco'])
 
-        if clientes.filter(endereco__bairro__nome__icontains = request.POST['endereco']):
-            clientes = clientes.filter(endereco__bairro__nome__icontains = request.POST['endereco'])
+        if clientes.filter(
+                endereco__bairro__nome__icontains=request.POST['endereco']):
+            clientes = clientes.filter(
+                endereco__bairro__nome__icontains=request.POST['endereco'])
 
-        if clientes.filter(endereco__bairro__cidade__nome__icontains = request.POST['endereco']):
-            clientes = clientes.filter(endereco__bairro__cidade__nome__icontains = request.POST['endereco'])
+        if clientes.filter(
+                endereco__bairro__cidade__nome__icontains=
+                request.POST['endereco']):
+            clientes = clientes.filter(
+                endereco__bairro__cidade__nome__icontains=
+                request.POST['endereco'])
 
-        if clientes.filter(endereco__bairro__cidade__uf__icontains = request.POST['endereco']):
-            clientes = clientes.filter(endereco__bairro__cidade__uf__icontains = request.POST['endereco'])
+        if clientes.filter(
+                endereco__bairro__cidade__uf__icontains=
+                request.POST['endereco']):
+            clientes = clientes.filter(
+                endereco__bairro__cidade__uf__icontains=
+                request.POST['endereco'])
 
     if request.POST.get('ativo', False):
         clientes = clientes.filter(ativo=False)
@@ -104,7 +120,7 @@ def filtrar(request):
     if request.POST['dataini'] and request.POST['datafim']:
         dataini = datetime.strptime(request.POST['dataini'], '%Y-%m-%d')
         datafim = datetime.strptime(request.POST['datafim'], '%Y-%m-%d')
-        clientes = clientes.filter(data_cadastro__range=[dataini,datafim])
+        clientes = clientes.filter(data_cadastro__range=[dataini, datafim])
 
     if not clientes:
         dados['clientes'] = Cliente.objects.all()
@@ -113,21 +129,26 @@ def filtrar(request):
 
     if request.POST.get('relatorio', False):
         dados['data'] = today
-        return render(request,template_relatorio,dados)
+        return render(request, template_relatorio, dados)
     else:
-        return render(request,template_home,dados)
+        return render(request, template_home, dados)
+
 
 def tabela(request):
-    return render(request,'tabela2.html')    
+    return render(request, 'tabela2.html')
+
 
 def relatorio(request):
-    pass    
+    pass
+
 
 def relatorios(request):
     pass
 
+
 def cartas_aniversario_home(request):
-    return render(request,'clientes/cartas_aniversario_home.html')
+    return render(request, 'clientes/cartas_aniversario_home.html')
+
 
 def cartas_aniversario_filtrar(request):
     dados = {}
@@ -135,35 +156,35 @@ def cartas_aniversario_filtrar(request):
     dia = request.POST['dia']
     carrega_parametros_carta_aniversario(request, dados)
 
-    clientes = Cliente.objects.filter(data_nascimento__month=mes)        
+    clientes = Cliente.objects.filter(data_nascimento__month=mes)
     dados['clientes'] = clientes
-        
+
     if dia != '0':
-        clientes = Cliente.objects.filter(data_nascimento__day=dia)  
+        clientes = Cliente.objects.filter(data_nascimento__day=dia)
 
     if request.POST.get('filtrar_imprimir', False):
         return render(request, 'clientes/cartas_aniversario.html', dados)
     else:
-        return render(request,'clientes/cartas_aniversario_home.html',dados)
+        return render(request, 'clientes/cartas_aniversario_home.html', dados)
 
-def cartas_aniversario_individual(request,id):
+
+def cartas_aniversario_individual(request, id):
     dados = {}
     cliente = Cliente.objects.filter(pk=id)
     dados['clientes'] = cliente
     carrega_parametros_carta_aniversario(request, dados)
     return render(request, 'clientes/cartas_aniversario.html', dados)
 
+
 def carrega_parametros_carta_aniversario(request, dados):
     modelo = request.POST['modelo']
-    
+
     if modelo == 'texto1':
         texto_carta = ParametrosGerais.objects.all()[0].texto_carta_aniv1
     elif modelo == 'texto2':
         texto_carta = ParametrosGerais.objects.all()[0].texto_carta_aniv2
     else:
         texto_carta = ParametrosGerais.objects.all()[0].texto_carta_aniv3
-
-    dados['url_logo'] = ParametrosGerais.objects.all()[0].url_logo
 
     dados['data'] = today
     dados['texto_carta'] = texto_carta
--- alfredo_imoveis/parametros/models.py
+++ alfredo_imoveis/parametros/models.py
@@ -3,18 +3,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from financeiro.models.conta_caixa import ContaCaixa
 
+
 class ParametrosGerais(models.Model):
     taxa_juros = models.DecimalField(max_digits=4, decimal_places=2)
     multa = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     texto_carta_aniv1 = models.TextField('Texto da carta de aniversário 1')
     texto_carta_aniv2 = models.TextField('Texto da carta de aniversário 2')
     texto_carta_aniv3 = models.TextField('Texto da carta de aniversário 3')
-    url_logo = models.CharField(max_length=150,verbose_name=u'URL da logo')
     clausula_contrato_adic = models.TextField('Cláusula de contrato adicional')
     gerente = models.ForeignKey(User, verbose_name="Usuário gerente",
-                                help_text="Informe o usuário gerente da empresa")
-    conta_caixa = models.ForeignKey(ContaCaixa, null=True, blank=True,
-                                    verbose_name='Conta caixa para financeiro de contratos')
-    
+                                help_text="Informe o gerente da empresa")
+    conta_caixa = models.ForeignKey(
+        ContaCaixa, null=True, blank=True,
+        verbose_name='Conta caixa para financeiro de contratos')
+
     def __unicode__(self):
         return unicode(self.id) + " - " + u"Parâmetros "
